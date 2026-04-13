669b65b9a7f63ef810257a0accbae759d940951c
yanyu4250 <3374481240@qq.com>
2026-04-13 14:51:44 +0800

    实现登录、注册页面，以及数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)       #逗号不能删除，必须传一个列表
