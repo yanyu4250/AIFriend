bdf55b7ffb043ca46a6942a3000fb605a8046761
yanyu4250 <3374481240@qq.com>
2026-04-20 10:38:25 +0800

    完善删除功能，创作按钮路由还原为创作页面

--- backend/web/views/create/character/remove.py
+++ backend/web/views/create/character/remove.py
@@ -1,8 +1,11 @@
+from os import remove
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from web.models.character import Character
+from web.views.utils.photo import remove_old_photo
 
 
 class RemoveCharacterView(APIView):
@@ -11,7 +14,10 @@ class RemoveCharacterView(APIView):
         try:
             character_id = request.data["character_id"]
             from web.models import character
-            Character.objects.filter(pk=character_id,author__user=request.user).delete()
+            character = Character.objects.get(pk=character_id,author__user=request.user)
+            remove_old_photo(character.photo)
+            remove_old_photo(character.background_image)
+            character.delete()
         except:
             return Response({
                 'result':'系统异常，请稍后重试'
