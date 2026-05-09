20565a572d43a13ad73c474fd2ff01d719541bb1
yanyu4250 <3374481240@qq.com>
2026-05-09 21:05:19 +0800

    将ai改造成流式输出并发送到前端

--- backend/web/views/message/chat/chat.py
+++ backend/web/views/message/chat/chat.py
@@ -1,4 +1,8 @@
+import json
+
+from django.http import StreamingHttpResponse
 from langchain_core.messages import HumanMessage
+from rest_framework.renderers import BaseRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -7,8 +11,16 @@ from web.models.friend import Friend
 from web.views.message.chat.graph import ChatGraph
 
 
+class SSERenderer(BaseRenderer):
+    media_type = 'text/event-stream'
+    format = 'txt'
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        return data
+
+
 class MessageChatView(APIView):
     permission_classes = [IsAuthenticated]
+    renderer_classes = [SSERenderer]
     def post(self, request):
         friend_id = request.data['friend_id']
         message = request.data['message'].strip()
@@ -27,9 +39,21 @@ class MessageChatView(APIView):
         inputs = {
             'messages':[HumanMessage(message)]
         }
-        res = app.invoke(inputs)
-        print(res['messages'][-1].content)
 
-        return Response({
-            'result': 'success',
-        })
+        def event_stream():
+            final_usage = {}
+            for msg, metadata in app.stream(inputs, stream_mode="messages"):
+                from langchain_core.messages import BaseMessageChunk
+                if isinstance(msg, BaseMessageChunk):
+                    if msg.content:
+                        yield f"data: {json.dumps({'content': msg.content}, ensure_ascii=False)}\n\n"
+                    if hasattr(msg, 'usage_metadata') and msg.usage_metadata:
+                        full_usage = msg.usage_metadata
+            yield "data: [DONE]\n\n"
+            print(full_usage)
+
+        response = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
+        response['Cache-Control'] = 'no-cache'
+        return  response
+
+
