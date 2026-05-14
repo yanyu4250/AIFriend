f90d112f912c3cfc891f6e90588cdabd45d52107
yanyu4250 <3374481240@qq.com>
2026-05-14 11:47:03 +0800

    添加知识库和知识检索工具

--- backend/web/views/message/memory/update.py
+++ backend/web/views/message/memory/update.py
@@ -34,10 +34,10 @@ def update_memory(friend):
         ]
     }
 
-    pprint(inputs)
+
     res = app.invoke(inputs)
     friend.memory = res['messages'][-1].content
-    pprint(friend.memory)
+
 
     friend.update_time = now()
     friend.save()
